a69fd7eb6c07dec68625d4ae0f925c17708e4457
Ephraim Beltran <git@beltran.cc>
2024-10-21 01:42:56 -0400

    style: add basic styling

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,3 +1,44 @@
+import { NavLink } from "react-router-dom";
 export function Header() {
-  return <h1>Artful</h1>;
+  return (
+    <header>
+      <h1>Artful</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/discover"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Discover
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              About Us
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
