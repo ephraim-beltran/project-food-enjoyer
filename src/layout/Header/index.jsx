65b2d2b08fd7814a32d16300a6f713a5764d47ef
Ephraim Beltran <git@beltran.cc>
2024-10-21 02:40:51 -0400

    feature: add Header section

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,44 +1,43 @@
 import { NavLink } from "react-router-dom";
+import styled from "./Header.module.css";
+
+const navList = [
+  { text: "Home", path: "/" },
+  { text: "Discover", path: "/discover" },
+  { text: "Contact", path: "/contact" },
+  { text: "About Us", path: "/about-us" },
+];
 export function Header() {
   return (
-    <header>
-      <h1>Artful</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/discover"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Discover
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Contact
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about-us"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              About Us
-            </NavLink>
-          </li>
+    <header className={styled.header}>
+      <h1 className={styled.h1}>Artful</h1>
+      <nav className={styled.nav}>
+        <ul className={styled.nav_list}>
+          {navList.map((item, index) => {
+            return (
+              <li key={`nav-list-${index + 1}`}>
+                <NavLink
+                  to={item.path}
+                  className={({ isActive }) =>
+                    isActive ? styled.active_nav : ""
+                  }
+                >
+                  {item.text}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </nav>
+      <label>
+        <select
+          name="language"
+          id="language"
+          className={styled.language_selector}
+        >
+          <option value="english">EN</option>
+        </select>
+      </label>
     </header>
   );
 }
