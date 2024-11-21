86a862f4585967b64f30b0601b8007fb20354e0f
Ephraim Beltran <git@beltran.cc>
2024-11-21 07:09:27 -0500

    feat(contact page): add simple contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,3 +1,15 @@
+import styled from "./Contact.module.css";
 export function Contact() {
-  return <h2>This is the Contacts page</h2>;
+  return (
+    <section>
+      <div className={styled.container}>
+        <h2>Contact Us</h2>
+        <div className={styled.name}>Green Routes</div>
+        <address>350 Victoria Street, Toronto, ON M5B 2K3</address>
+        <a href="mailto:greenroutes@gmail.com" l>
+          greenroutes@gmail.com
+        </a>
+      </div>
+    </section>
+  );
 }
